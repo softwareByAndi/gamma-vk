57ccd05bb9d774645c5f4c5c130aca9a4581b40a
softwareByAndi <softwareByAndi@gmail.com>
2025-06-26 00:14:18 -0600

    remove deprecated integration test file

--- tests/integration_test.rs
+++
@@ -1,9 +0,0 @@
-//! Integration tests for Gamma-VK library
-//!
-//! These tests verify that the library works correctly as a whole,
-//! testing the public API from an external perspective.
-
-#![allow(unused_imports, dead_code, unused_variables)]
-use gamma_vk::{GammaVkError, VertexBuffer, VulkanContext};
-use std::sync::Arc;
-
